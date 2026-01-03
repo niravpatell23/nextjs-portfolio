d6e35c2873a274e2cf61fff0a858fb5b5f4c76d7
niravpatell23 <nnirav.patel7@gmail.com>
2026-01-03 14:38:45 -0500

    Update dependencies to latest versions and Node.js 20+

--- components/StarBg.tsx
+++ components/StarBg.tsx
@@ -10,7 +10,7 @@ import { useTheme } from "@/context/theme-context";
 
 const StarBackground = (props: any) => {
   const { theme } = useTheme();
-  const ref: any = useRef();
+  const ref: any = useRef(null);
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
@@ -30,7 +30,7 @@ const StarBackground = (props: any) => {
           color={color}
           size={0.002}
           sizeAttenuation={true}
-          dethWrite={false}
+          depthWrite={false}
         />
       </Points>
     </group>
